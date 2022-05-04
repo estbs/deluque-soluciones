03630c339538e8e4c739d1d1984061b847042f60
Esteban BS <estbs93@gmail.com>
2022-05-03 20:26:42 -0500

    Feat: Add show action

--- spec/requests/suppliers_spec.rb
+++ spec/requests/suppliers_spec.rb
@@ -22,4 +22,22 @@ RSpec.describe SuppliersController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET show' do
+    before { get :show, params: params }
+
+    let(:params) do
+      { id: supplier.id }
+    end
+
+    let(:supplier) { create(:supplier) }
+
+    it 'Assigns @word' do
+      expect(assigns(:supplier)).to eq(supplier)
+    end
+
+    it 'Renders the show template' do
+      expect(response).to render_template(:show)
+    end
+  end
 end
