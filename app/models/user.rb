6c4180bd97fb9c6475fa194694ba48210b10cad5
Esteban BS <estbs93@gmail.com>
2022-05-09 17:20:33 -0500

    Feat: Fix test using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :name, :identification_type, :identification_number, :email, presence: true
 end
