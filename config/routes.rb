d7b5a51c7431e5e997a21efc1ca56d1327eb57b7
Esteban BS <estbs93@gmail.com>
2022-05-09 14:54:02 -0500

    Feat: Add devise gem
    Set configuration to devise and rails 7. Charge the views and controllers from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
