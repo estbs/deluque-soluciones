2daa2c865b6cb6ad1fcee011a555b1a7afdc0db8
Esteban BS <estbs93@gmail.com>
2022-05-30 22:52:02 -0500

    Feat: Change association between suppliers and qualities

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,9 +2,7 @@ class Supplier < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :address
-
-  has_many :supplier_qualities, dependent: :destroy
-  has_many :qualities, through: :supplier_qualities
+  has_and_belongs_to_many :qualities
 
   accepts_nested_attributes_for :address
 end
