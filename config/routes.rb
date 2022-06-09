657d94117f8178ccd4d9b17cebac83d57114b143
Esteban BS <estbs93@gmail.com>
2022-06-08 23:59:46 -0500

    Feat: Add dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'suppliers#index'
+  root 'dashboard#index'
 
   resources :suppliers
   resources :users, only: %i[index show edit update destroy]
